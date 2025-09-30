756fb7913861caece964d15fc70825ab2b2674ce
alvaranz83 <103906142+alvaranz83@users.noreply.github.com>
2025-09-30 14:16:12 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from app.routers import jobs, candidates, health
+
+app = FastAPI(title="Job Posting Service")
+
+# Routers
+app.include_router(jobs.router, prefix="/jobs", tags=["Jobs"])
+app.include_router(candidates.router, prefix="/candidates", tags=["Candidates"])
+app.include_router(health.router, prefix="/health", tags=["Health"])
 
